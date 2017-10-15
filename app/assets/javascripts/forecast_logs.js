814386bda3f9e94b3e34cb7cf24453fc698ba07e
Denis Bereza <den.intellcom@gmail.com>
2017-10-15 18:13:38 +0300

    Process AJAX response in forecast_logs controller

--- app/assets/javascripts/forecast_logs.js
+++ app/assets/javascripts/forecast_logs.js
@@ -9,9 +9,24 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     google.maps.event.addListener(autocomplete, 'place_changed', function() {
         var place = new City(autocomplete.getPlace(), input.value);
-
-        console.log("HTTP::API::CALL");
-        console.log(place);
+        postForecastLog(place);
     });
-
 });
+
+function postForecastLog(city) {
+    var options = {
+        method: "post",
+        headers: {
+            'Content-type': 'application/json',
+            'X-CSRF-Token': document.querySelector('[name="csrf-token"]').content
+        },
+        credentials: 'same-origin',
+        body: JSON.stringify(city)
+    };
+
+    fetch('/forecast_logs', options)
+        .then(function(response) {
+            console.log(response.json());
+        })
+        .catch( console.error );
+}
--- app/controllers/forecast_logs_controller.rb
+++ app/controllers/forecast_logs_controller.rb
@@ -1,2 +1,12 @@
 class ForecastLogsController < ApplicationController
+
+  def create
+    render :json => { :success => true, :logs => [{temp_min: 32, temp_max: 44, probPrec: 0.4}] }
+  end
+
+  private
+
+  def permited_params
+    @permit_params ||= params.permit(:id, :lng, :lat, :localeFullName)
+  end
 end
