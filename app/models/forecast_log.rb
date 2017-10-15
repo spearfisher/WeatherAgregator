a672ee9d1ea01ca53160e12b55f597a3785daf9b
Denis Bereza <den.intellcom@gmail.com>
2017-10-15 16:47:03 +0300

    Add ForecastLog model

---
+++ app/models/forecast_log.rb
@@ -0,0 +1,10 @@
+class ForecastLog
+  include Mongoid::Document
+  include Mongoid::Timestamps::Created
+
+  field :session_id, type: String
+  field :city, type: Hash
+  field :forecasts, type: Array
+
+  index({ session_id: 1 }, { name: "session_id_index" })
+end
