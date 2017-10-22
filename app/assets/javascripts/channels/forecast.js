d90db207d960bb54d3e45a8537f03f5edcb03245
Denis Bereza <den.intellcom@gmail.com>
2017-10-23 01:46:33 +0300

    Check for existing log before request.

--- app/assets/javascripts/channels/forecast.js
+++ app/assets/javascripts/channels/forecast.js
@@ -2,6 +2,11 @@ App.forecast = App.cable.subscriptions.create("ForecastChannel", {
   received: function(data) {
       console.log("!!!!! received !!!!!!!");
       console.log(data);
+
+      var input = document.getElementById('searchTextField');
+      input.readOnly = false;
+
+      //TODO: render new forecast log
   },
 
   fetchForecast: function(city) {
--- app/assets/javascripts/forecast_logs.js
+++ app/assets/javascripts/forecast_logs.js
@@ -9,6 +9,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     google.maps.event.addListener(autocomplete, 'place_changed', function() {
         var city = new City(autocomplete.getPlace(), input.value);
-        App.forecast.fetchForecast(city);
+
+        console.log(city);
+        var existed_log = document.getElementById(city.id)
+
+        if (existed_log == null
+            || parseInt((new Date - new Date(existed_log.dataset.created)) / (1000 * 3600)) > 4) {
+
+            input.readOnly = true;
+            App.forecast.fetchForecast(city);
+        } else {
+            existed_log.scrollIntoView();
+        }
     });
 });
